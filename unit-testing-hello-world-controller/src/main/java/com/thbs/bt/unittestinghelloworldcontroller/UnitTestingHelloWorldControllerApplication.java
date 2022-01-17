cac1c9b76a12fa5706c257c88625b7f9ab648a47
SheelaBheemaraya <sb3891604@gmail.com>
2022-01-17 14:17:09 +0530

    juint for hello world controller

---
+++ unit-testing-hello-world-controller/src/main/java/com/thbs/bt/unittestinghelloworldcontroller/UnitTestingHelloWorldControllerApplication.java
@@ -0,0 +1,13 @@
+package com.thbs.bt.unittestinghelloworldcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingHelloWorldControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestingHelloWorldControllerApplication.class, args);
+	}
+
+}
---
+++ unit-testing-hello-world-controller/src/test/java/com/thbs/bt/unittestinghelloworldcontroller/UnitTestingHelloWorldControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thbs.bt.unittestinghelloworldcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingHelloWorldControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
